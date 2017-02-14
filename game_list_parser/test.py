64ccff5610604de67847c0d9ac96f9b24763ffe8
이도준 <leedoe@naver.com>
2017-02-15 01:20:37 +0900

    this commit is that trying to parse game list in steam web site

--- game_list_parser/test.py
+++ game_list_parser/test.py
@@ -1,63 +1,139 @@
+# -*- coding: utf-8 -*-
 import urllib.request
+import codecs
+import json
 from bs4 import BeautifulSoup
 
-#online game list
+file = codecs.open("GameList.txt", 'a', 'utf-8')
+
+# online game list
+"""
 req = urllib.request.Request(
-	'https://namu.wiki/w/%EC%98%A8%EB%9D%BC%EC%9D%B8%20%EA%B2%8C%EC%9E%84/%EB%AA%A9%EB%A1%9D',
-	data=None,
-	headers={
-		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393'
-	}
+    'https://namu.wiki/w/%EC%98%A8%EB%9D%BC%EC%9D%B8%20%EA%B2%8C%EC%9E%84/%EB%AA%A9%EB%A1%9D',
+    data=None,
+    headers={
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393'
+    }
 )
 
 f = urllib.request.urlopen(req).read().decode('utf-8')
 
 bs = BeautifulSoup(f, 'lxml').find_all('a', class_='wiki-link-internal')
 
-"""
 print("########## ONLINE GAME LIST ########")
 for item in bs:
-	print(item.text)
+    print(item.text)
 """
 
-#mobile game list
+# mobile game list
+"""
 req = urllib.request.Request(
-	'https://namu.wiki/w/%EB%AA%A8%EB%B0%94%EC%9D%BC%20%EA%B2%8C%EC%9E%84/%EB%AA%A9%EB%A1%9D',
-	data=None,
-	headers={
-		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393'
-	}
+    'https://namu.wiki/w/%EB%AA%A8%EB%B0%94%EC%9D%BC%20%EA%B2%8C%EC%9E%84/%EB%AA%A9%EB%A1%9D',
+    data=None,
+    headers={
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393'
+    }
 )
 
 f = urllib.request.urlopen(req).read().decode('utf-8')
 
 bs = BeautifulSoup(f, 'lxml').find_all('ul', class_='wiki-list')
 
-"""
 print("########## MOBILE GAME LIST ########")
 for item in bs:
-	temp = item.find_all('a', class_='wiki-link-internal')
-	for item2 in temp:
-		print(item2.text)
-	
+    temp = item.find_all('a', class_='wiki-link-internal')
+    for item2 in temp:
+        print(item2.text)
 """
 
 
-#playstation game list
+# playstation game list
+"""
 req = urllib.request.Request(
-	'https://namu.wiki/w/%ED%94%8C%EB%A0%88%EC%9D%B4%EC%8A%A4%ED%85%8C%EC%9D%B4%EC%85%98/%EA%B2%8C%EC%9E%84%20%EB%AA%A9%EB%A1%9D',
-	data=None,
-	headers={
-		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393'
-	}
+    'https://namu.wiki/w/%ED%94%8C%EB%A0%88%EC%9D%B4%EC%8A%A4%ED%85%8C%EC%9D%B4%EC%85%98%204/%EA%B2%8C%EC%9E%84%20%EB%AA%A9%EB%A1%9D',
+    data=None,
+    headers={
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393'
+    }
 )
 
 f = urllib.request.urlopen(req).read().decode('utf-8')
 
 bs = BeautifulSoup(f, 'lxml').find_all('ul', class_='wiki-list')
 
+gameListArray = []
 print("########### PLAYSTATION GAME LIST ########")
 for item in bs:
-	temp = item.find_all('p')
-	for item2 in temp:
-		print(item2.text)
+    temp = item.find_all('p')
+    for item2 in temp:
+        gameName = item2.text.replace(" ☆", "")
+        gameName = gameName.replace(" ★", "")
+        if gameName[-1] == ' ':
+            gameName = gameName[0: -1]
+        gameListArray.append(gameName)
+
+print(gameListArray)
+for item in gameListArray:
+    file.write(item + '\n')
+"""
+
+"""
+# test
+req = urllib.request.Request(
+    'https://namu.wiki/edit/%ED%94%8C%EB%A0%88%EC%9D%B4%EC%8A%A4%ED%85%8C%EC%9D%B4%EC%85%98%204/%EA%B2%8C%EC%9E%84%20%EB%AA%A9%EB%A1%9D?section=2',
+    data=None,
+    headers={
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393'
+    }
+)
+
+f = urllib.request.urlopen(req).read().decode('utf-8')
+bs = BeautifulSoup(f, 'lxml').find('textarea', {"id": "textInput"})
+
+# print(f)
+print(bs.text)
+"""
+
+"""
+req = urllib.request.Request(
+    'http://api.steampowered.com/ISteamApps/GetAppList/v0002/',
+    data=None,
+    headers={
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393'
+    }
+)
+
+f = urllib.request.urlopen(req).read().decode('utf-8')
+
+bs = BeautifulSoup(f, 'lxml').find_all('apps')
+
+data = json.loads(str(f))
+count = 0
+for item in data['applist']['apps']:
+    count = count+1
+    file.write(item['name'] + '\n')
+"""
+
+# game list in steam
+req = None
+
+for i in range(1, 1051):
+    print(i)
+    req = urllib.request.Request(
+        'http://store.steampowered.com/search/?page=' + str(i),
+        data=None,
+        headers={
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393'
+        }
+    )
+
+    f = urllib.request.urlopen(req).read().decode('utf-8')
+
+    bs = BeautifulSoup(f, 'lxml').find('div', class_='leftcol large')
+    bs = bs.find('div', {"id": 'search_result_container'})
+    bs = bs.find_all('div')[1]
+    bs = bs.find_all('a', class_='search_result_row ds_collapse_flag')
+
+    for item in bs:
+        title = item.find('span', class_='title')
+        file.write(title.text + '\n')
