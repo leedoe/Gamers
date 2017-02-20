78374a45771fda7944c498932d3de4ec60d32bf7
이도준 <leedoe@naver.com>
2017-02-21 01:04:13 +0900

    I made django application and then defined model

--- Webapp/reviewer/settings.py
+++ Webapp/reviewer/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Gamers',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +79,16 @@ WSGI_APPLICATION = 'reviewer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'Gamer',
+        'USER': 'leedoe',
+        'PASSWORD': 'ehwns0910',
+        'HOST': 'gamereview.cofbl2jproi3.us-west-2.rds.amazonaws.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
