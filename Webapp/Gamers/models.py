73330e02c673dea93c4e9aaffa069192b4f15cbb
이도준 <leedoe@naver.com>
2017-02-22 00:46:10 +0900

    there is a problem about migrate

--- Webapp/Gamers/models.py
+++ Webapp/Gamers/models.py
@@ -1,17 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 class Game(models.Model):
-    name = models.CharField()
-    developer = models.CharField()
-    publisher = models.CharField()
+    name = models.CharField(max_length = 100)
+    developer = models.CharField(max_length = 100)
+    publisher = models.CharField(max_length = 100)
     release_date = models.DateTimeField()
-    platform = models.CharField()
-    genre = models.CharField()
-    homepage = models.CharField()
-    picture = models.CharField()
+    platform = models.CharField(max_length = 30)
+    genre = models.CharField(max_length = 100)
+    homepage = models.CharField(max_length = 100)
+    picture = models.CharField(max_length = 100)
 
     def __str__(self):
         return self.title
+
+
+class Gamer(User):
+    def __str__(self):
+        return self.username
