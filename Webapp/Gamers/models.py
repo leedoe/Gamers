c87324a85bcd1dcca99a300f5c18fccac1d0b06d
이도준 <leedoe@naver.com>
2017-02-23 15:13:38 +0900

    already made models and success Oauth

--- Webapp/Gamers/models.py
+++ Webapp/Gamers/models.py
@@ -4,15 +4,72 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+class Developer(models.Model):
+    game = models.ForeignKey(
+        'Game',
+        on_delete = models.CASCADE,
+        )
+    developer_name = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.developer_name
+
+
+class Publisher(models.Model):
+    game = models.ForeignKey(
+        'Game',
+        on_delete = models.CASCADE
+        )
+    publisher_name = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.publisher_name
+
+
+class Platform(models.Model):
+    PLATFORM_LIST_CHOICES = (
+        ('WINDOWS', 'Windows'),
+        ('MAC', 'Mac'),
+        ('LINUX', 'LINUX'),
+        ('PS4', 'PLAYSTATION 4'),
+        ('XBOX ONE', 'XBOX ONE'),
+        ('PS3', 'PLAYSTATION 3'),
+        ('XBOX 360', 'XBOX 360')
+
+    )
+    game = models.ForeignKey(
+        'Game',
+        on_delete = models.CASCADE
+        )
+    publisher_name = models.CharField(
+        max_length = 20,
+        choices = PLATFORM_LIST_CHOICES,
+        )
+
+    def __str__(self):
+        return self.publisher_name
+
+
+class Genre(models.Model):
+    game = models.ForeignKey(
+        'Game',
+        on_delete = models.CASCADE
+        )
+    genre_name = models.CharField(max_length = 20)
+
+
+class Pictures(models.Model):
+    game = models.ForeignKey(
+        'Game',
+        on_delete = models.CASCADE
+        )
+    picture_url = models.URLField()
+
+
 class Game(models.Model):
     name = models.CharField(max_length = 100)
-    developer = models.CharField(max_length = 100)
-    publisher = models.CharField(max_length = 100)
     release_date = models.DateTimeField()
-    platform = models.CharField(max_length = 30)
-    genre = models.CharField(max_length = 100)
-    homepage = models.CharField(max_length = 100)
-    picture = models.CharField(max_length = 100)
+    homepage = models.URLField()
 
     def __str__(self):
         return self.title
