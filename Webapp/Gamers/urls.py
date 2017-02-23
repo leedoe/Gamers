ace10d36d59ae61a51769e16ab3f7372a42244da
이도준 <leedoe@naver.com>
2017-02-23 15:13:38 +0900

    already made models and success Oauth

--- Webapp/Gamers/urls.py
+++ Webapp/Gamers/urls.py
@@ -17,4 +17,6 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
+	url(r'^$', views.test),
+	url(r'', include('social_django.urls', namespace='social')),
 ]
--- Webapp/Gamers/views.py
+++ Webapp/Gamers/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 from django.template.context import RequestContext
 
 # Create your views here.
+
+def test(request):
+	return render(request, 'Gamers/test.html', {})
