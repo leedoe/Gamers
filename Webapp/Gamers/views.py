4bc9c44e7ef8877ac469505dce229005772d6184
이도준 <leedoe@naver.com>
2017-03-01 00:32:52 +0900

    can show picture with using graph API

--- Webapp/Gamers/views.py
+++ Webapp/Gamers/views.py
@@ -5,11 +5,9 @@ from django.template.context import RequestContext
 
 
 def test(request):
-	user = request.user
-	
-	try:
-		facebook_login = user.social_auth.get(provider='facebook')
-	except:
-		facebook_login = None
+    user = request.user
 
-	return render(request, 'Gamers/test.html', {'facebook_login': facebook_login,})
+    facebook_login = user.social_auth.get(provider='facebook')
+    picture_url = 'http://graph.facebook.com/v2.8/' + facebook_login.uid + '/picture?type=square&hight=600&width=600&return_ssl_resources=1'
+
+    return render(request, 'Gamers/test.html', {'facebook_login': facebook_login, "picture_url": picture_url})
