0c032772fa7d2e17b56f5ccffaacdfe64b650978
이도준 <leedoe@naver.com>
2017-04-30 16:23:06 +0900

    tagsinput 완성, 실제로 DB에 저장하는 기능 필요함

--- Webapp/Gamers/forms.py
+++ Webapp/Gamers/forms.py
@@ -1,7 +1,14 @@
 from django.forms import ModelForm
+from Gamers.widgets.tagsinput import TagsInputWidget
 from .models import Game
 
 class GameForm(ModelForm):
 	class Meta:
 		model = Game
 		fields = ['title', 'release_date', 'homepage', 'developers', 'publishers', 'platforms', 'genres']
+		widgets = {
+			'developers': TagsInputWidget,
+			'publishers': TagsInputWidget,
+			'platforms': TagsInputWidget,
+			'genres': TagsInputWidget,
+		}
