502fc55b55982c3b1112719a04f3cab5ad2f521e
이도준 <leedoe@naver.com>
2017-05-17 01:21:03 +0900

    게임목록 출력기능 추가(view, template, url)

--- Webapp/Gamers/urls.py
+++ Webapp/Gamers/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     url(r'^main$', views.main, name='main'),
     url(r'^register$', views.register_game, name='register_game'),
     url(r'^game/(?P<game_id>\d+)$', views.game_viewer, name='game_viewer'),
+    url(r'^gamelist$', views.game_list, name='game_list'),
     url(r'', include('social_django.urls', namespace='social')),
 ]
