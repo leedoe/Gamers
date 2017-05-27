73c5f024dc5a49d87e61f5a9a965cf5148511e59
이도준 <leedoe@naver.com>
2017-05-28 00:37:10 +0900

    크롤링 파일 함수화

--- Webapp/steam.py
+++ Webapp/steam.py
@@ -10,191 +10,172 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE',"reviewer.settings")
 
 import django
 django.setup()
-from Gamers.models import Game, Developer, Genre, Publisher, Platform
-
-# game list in steam
-req = None
-
-for i in range(1, 1183):
-    print(i)
-    req = urllib.request.Request(
-        'http://store.steampowered.com/search/?page=' + str(i),
-        data=None,
-        headers={
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393'
-        }
-    )
-    # proxy_host = '154.16.126.94'
-    # req.set_proxy(proxy_host, 'http')
-
-    f = urllib.request.urlopen(req).read().decode('utf-8')
-
-    bs = BeautifulSoup(f, 'lxml').find('div', class_='leftcol large')
-    bs = bs.find('div', {"id": 'search_result_container'})
-    bs = bs.find_all('div')[1]
-    bs = bs.find_all('a', class_='search_result_row ds_collapse_flag')
-
-    for item in bs:
-        content = {}
-
-        title = item.find('span', class_='title').text
-        content['title'] = title
-
-        platforms_list = []
-        platform = ""
-
-        if item.find('span', class_='platform_img win'):
-            platforms_list.append('Windows')
-        if item.find('span', class_='platform_img mac'):
-            platforms_list.append('Mac')
-        if item.find('span', class_='platform_img linux'):
-            platforms_list.append('Linux')
-
-        platform = ','.join(platforms_list)
-
-        content['platforms'] = platform
+from Gamers.models import Game, Developer, Genre, Publisher, Platform, Screenshot
 
 
-        # file.write(title.text + '\n')
-        # print(title, end="-----")
-        # print(str(platform['Win']) + str(platform['Mac']) + str(platform['Linux']), end='-----')
-        # print(item['data-ds-appid'])
+# stroe.steampowered 페이지에 있는 게임 목록을 가져옴
+def get_appid_steam(page_number):
+    appid = []
+    for i in range(1, page_number):
+        req = urllib.request.Request(
+            'http://store.steampowered.com/search/?page=' + str(i),
+            data=None,
+            headers={
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393'
+            }
+        )
 
-        req2 = urllib.request.Request(
-            'http://store.steampowered.com/app/' + str(item['data-ds-appid']),
+        f = urllib.request.urlopen(req).read().decode('utf-8')
+
+        bs = BeautifulSoup(f, 'lxml')
+        appid_row = bs.find_all('a', class_='search_result_row ds_collapse_flag')
+    
+        for item in appid_row:
+            appid.append(item['data-ds-appid'])
+    
+    return appid
+
+
+# 영어로 된 달을 숫자로 바꿈
+def convert_release_date(release_date_raw):
+    mdy = release_date_raw.split()
+    
+    day = mdy[0]
+    month = mdy[1]
+    year = mdy[2]
+
+    for month_name, value in {'Jan,': '01', 'Feb,':'02', 'Mar,':'03', 'Apr,':'04', 'May,':'05', 'Jun,':'06', 'Jul,':'07', 'Aug,':'08', 'Sep,':'09', 'Oct,':'10', 'Nov,':'11', 'Dec,':'12'}.items():
+        if month == month_name:
+            month = value
+            break
+
+    release_date = year + '-' + month + '-' + day
+
+    return release_date
+
+
+# 스팀에서 게임 데이터를 가져옴(title, release_date, homepage, genre, developer, publisher, screenshot)
+def get_game_data(appid):
+    content_list = []
+    num = 1
+    for item in appid:
+        url = 'http://store.steampowered.com/app/' + str(item)
+        req = urllib.request.Request(
+            url,
             data=None,
             headers={
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393',
                 'cookie': 'browserid=1344725717332693116; sessionid=adcfbe925844f84782bd1577; strResponsiveViewPrefs=touch; birthtime=678985201; lastagecheckage=9-July-1991; recentapps=%7B%22271590%22%3A1487220706%7D; timezoneOffset=32400,0; _ga=GA1.2.657082707.1487220674; mature_content=1'
             }
         )
-        # req2.set_proxy(proxy_host, 'http')
 
-        f = urllib.request.urlopen(req2).read().decode('utf-8')
+        f = urllib.request.urlopen(req).read().decode('utf-8')
         bs = BeautifulSoup(f, 'lxml')
 
-        # get game information
-        gameInfoBlock = bs.find_all('div', class_='details_block')
+        title = bs.find('div', class_='apphub_AppName').text
+        if Game.objects.filter(title=title).exists():
+            print(str(num) + '/' + str(len(appid)))
+            num = num + 1
+            continue
 
-        if len(gameInfoBlock) == 5:
-            WebsiteBlock = gameInfoBlock[4]
-            gameInfoBlock = gameInfoBlock[3]
+        release_date = convert_release_date(bs.find('span', class_='date').text)
+        homepage_raw = bs.find('a', class_='linkbar', attrs={'rel':'noreferrer'})
+        if homepage_raw is not None:
+            homepage = homepage_raw['href']
         else:
-            WebsiteBlock = gameInfoBlock[1]
-            gameInfoBlock = gameInfoBlock[0]
+            homepage = ''
 
-        genre = gameInfoBlock.find_all('a', attrs={"href": re.compile('http://store.steampowered.com/genre/[.]*')})
-        developer = gameInfoBlock.find_all('a', attrs={"href": re.compile('http://store.steampowered.com/search/\?developer[.]*')})
-        publisher = gameInfoBlock.find_all('a', attrs={"href": re.compile('http://store.steampowered.com/search/\?publisher[.]*')})
+        platforms = []
+        if bs.find('span', class_='platform_img win'):
+            platforms.append('Windows')
+        if bs.find('span', class_='platform_img mac'):
+            platforms.append('Mac')
+        if bs.find('span', class_='platform_img linux'):
+            platforms.append('Linux')
 
-        website = WebsiteBlock.find('a')['href']
+        content_div = bs.find_all('div', class_='page_content')
+        for page in content_div:
+            if page['class'][0] == 'page_content':
+                genres_raw = page.find_all('a', attrs={"href": re.compile('http://store.steampowered.com/genre/[.]*')})
+                developers_raw = page.find_all('a', attrs={"href": re.compile('http://store.steampowered.com/search/\?developer[.]*')})
+                publishers_raw = page.find_all('a', attrs={"href": re.compile('http://store.steampowered.com/search/\?publisher[.]*')})
+                break
 
-        release_date_raw = bs.find('span', class_='date').text
-        mdy = release_date_raw.split()
+        genres = []
+        developers = []
+        publishers = []
         
-        day = mdy[0]
-        month = mdy[1]
-        year = mdy[2]
+        for g in genres_raw:
+            genres.append(g.text.strip())
 
-        month = month.replace('Jan,', '01')
-        month = month.replace('Feb,', '02')
-        month = month.replace('Mar,', '03')
-        month = month.replace('Apr,', '04')
-        month = month.replace('May,', '05')
-        month = month.replace('Jun,', '06')
-        month = month.replace('Jul,', '07')
-        month = month.replace('Aug,', '08')
-        month = month.replace('Sep,', '09')
-        month = month.replace('Oct,', '10')
-        month = month.replace('Nov,', '11')
-        month = month.replace('Dec,', '12')
+        for d in developers_raw:
+            developers.append(d.text.strip())
+        
+        for p in publishers_raw:
+            publishers.append(p.text.strip())
 
-        release_date = year + '-' + month + '-' + day
-        #print(release_date)
+        screenshot = bs.find('a', class_='highlight_screenshot_link')['href'][43:].replace('1920x1080', '600x338')
         
 
-
-        # print('GENRE: ', end='')
-        genres_list = []
-        genres = ''
-        for genreName in genre:
-            # print(genreName.text, end=", ")
-            temp = genreName.text
-            temp.replace(',', ' ')
-            genres_list.append(temp)
-
-        genres = ','.join(genres_list)
-
-        #print('DEVELOPER: ', end='')
-        developers_list = []
-        developers = ''
-        for developerName in developer:
-            # print(developerName.text, end=', ')
-            temp = developerName.text
-            temp.replace(',', ' ')
-            developers_list.append(temp)
-
-        developers = ','.join(developers_list)
-
-
-        # print('PUBLISHER: ', end='')  
-        publishers_list = []
-        publishers = ''
-        for publisherName in publisher:
-            # print(publisherName.text, end=', ')
-            temp = publisherName.text
-            temp.replace(',', ' ')
-            publishers_list.append(temp)
-
-        publishers = ','.join(publishers_list)
-
-        # print(website)
-
-        obj = Game(
-            title=title,
-            release_date = release_date
-        )
-        #print(title)
+        content = {}
+        content['title'] = title
+        content['release_date'] = release_date
+        content['homepage'] = homepage
+        content['platforms'] = platforms
         content['genres'] = genres
         content['developers'] = developers
         content['publishers'] = publishers
-        content['release_date'] = release_date
-        if website.find('http://store.steampowered') == -1:
-            content['homepage'] = website
-            obj.homepage = website
+        content['screenshot'] = screenshot
 
-        try:
-            obj.save()
-        except:
+        content_list.append(content)
+
+        print(str(num) + '/' + str(len(appid)))
+        num = num + 1
+    return content_list
+
+
+# Game모델 저장(genre, developer, publisher 관계연결), Screenshot모델 저장
+def save_object(content):
+    for item in content:
+        obj, created = Game.objects.update_or_create(
+            title = item['title'],
+            release_date = item['release_date'],
+            homepage = item['homepage'],
+        )
+
+        if created == False:
+            print('exist already')
             continue
 
-        for item in developers_list:
-            temp, created = Developer.objects.get_or_create(name=item)
-            obj.developers.add(temp)
+        
+        obj.save()
 
-        for item in publishers_list:
-            temp, created = Publisher.objects.get_or_create(name=item)
-            obj.publishers.add(temp)
-
-        for item in platforms_list:
-            temp, created = Platform.objects.get_or_create(name=item)
+        for platform in item['platforms']:
+            temp, created = Platform.objects.get_or_create(name=platform)
             obj.platforms.add(temp)
 
-        for item in genres_list:
-            temp, created = Genre.objects.get_or_create(name=item)
+        for genre in item['genres']:
+            temp, created = Genre.objects.get_or_create(name=genre)
             obj.genres.add(temp)
+
+        for developer in item['developers']:
+            temp, created = Developer.objects.get_or_create(name=developer)
+            obj.developers.add(temp)
+
+        for publisher in item['publishers']:
+            temp, created = Publisher.objects.get_or_create(name=publisher)
+            obj.publishers.add(temp)
+
+        screenshot = Screenshot.objects.get_or_create(screenshot_url=item['screenshot'], game=obj)
+
+        print('Save object!')
+
         
 
-        # print(content)
-        # client = requests.session()
-        # client.get('http://127.0.0.1:8000/gamers/register/')
-        # csrftoken = client.cookies['csrftoken']
-        # content['csrfmiddlewaretoken'] = csrftoken
+        
 
-        # client.post('http://127.0.0.1:8000/gamers/register/', data=content, headers=dict(Referer='http://127.0.0.1:8000/gamers/register/'))
-        #res = requests.post('http://127.0.0.1:8000/gamers/register/', data = content)
-        # get screenshot
-        # gameScreenshot = bs.find('a', class_='highlight_screenshot_link')
-        # filename = re.sub('[\W]', '', title)
-        # urllib.request.urlretrieve(gameScreenshot['href'][43:], os.path.join('D:\\git\\Gamers\\game_list_parser\\pictures', filename + '.jpg'))
+    
 
+
+game_list = get_game_data(get_appid_steam(10))
+save_object(game_list)
