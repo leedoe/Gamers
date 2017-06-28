78a7625a433023aa87615fda1013637896ecc9a4
이도준 <leedoe@naver.com>
2017-06-28 22:29:43 +0900

    profile에 내가 평가한 게임 별점과 그 게임의 별점 출력하게 수정

--- Webapp/accounts/views.py
+++ Webapp/accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.views import login as auth_login
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
+from django.db.models import Avg
 from allauth.socialaccount.models import SocialApp
 from allauth.socialaccount.templatetags.socialaccount import get_providers
 from .forms import UsernameEditForm
@@ -27,10 +28,14 @@ def login(request):
 
 @login_required
 def profile(request):
+    myreviewlist = []
     myreview = Review.objects.filter(user=request.user)
-    print(myreview)
+    for review in myreview:
+        gamescore = Review.objects.filter(game=review.game).aggregate(Avg('score'))['score__avg']
+        myreviewlist.append({'myreview': review, 'gamescore':gamescore})
 
-    return render(request, 'accounts/profile.html', {'myreview': myreview})
+
+    return render(request, 'accounts/profile.html', {'myreviewlist': myreviewlist})
 
 
 @login_required
